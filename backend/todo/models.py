254af40c1721804eebc267ce96f61e1e7754338b
Luis Pedraza <luispedraza@gmail.com>
2021-01-02 13:31:09 +0100

    creado el modelo para el elemento todo y aplicadas las migraciones

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo (models.Model):
+    title = models.CharField(max_length=120)    # Título del recordatorio
+    description = models.TextField()            # Contenido del recordatorio
+    completed = models.BooleanField(default=False)  # Indica si está completada
+
+    def __str__(self):
+        return self.title
